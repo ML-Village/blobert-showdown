65ddb95e21619930d84b8a84bbcc53b5835dce4f
0xhatsume <0xhatsume@gmail.com>
2024-04-23 00:20:21 +0800

    🔨 Update dojo auth scripts

--- dojo-starter/scripts/default_auth.sh
+++ dojo-starter/scripts/default_auth.sh
@@ -12,8 +12,12 @@ echo "--------------------------------------------------------------------------
 
 # enable system -> models authorizations
 sozo auth grant --world $WORLD_ADDRESS --wait writer \
-  Position,dojo_starter::systems::actions::actions\
-  Moves,dojo_starter::systems::actions::actions\
+  Player,dojo_starter::systems::lobby::lobby\
+  Lineup,dojo_starter::systems::lobby::lobby\
+  Game,dojo_starter::systems::lobby::lobby\
+  BlobertDex,dojo_starter::systems::registry::registry\
+  MoveBook,dojo_starter::systems::registry::registry\
+  Gen1RandomMoveSet,dojo_starter::systems::registry::registry\
   >/dev/null
 
 echo "Default authorizations have been successfully set."
